48e87f2276e8fb0f95086de7757909bf0eed5d7b
shrikantmsd <shrikantmsd@gmail.com>
2026-05-26 01:35:57 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const path = require('path');
-const nextConfig = {
-  reactStrictMode: false,
-  turbopack: {
-    root: path.resolve(__dirname),
-  },
-};
-module.exports = nextConfig;
